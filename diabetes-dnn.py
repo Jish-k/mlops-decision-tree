97b4040789e1bf8145eb97aceb598881e43cb9c3
Jishnu7676 <jishnuk7676@gmail.com>
2026-02-18 22:00:05 +0530

    Fixed dataset filename in DNN script

--- diabetes-dnn.py
+++ diabetes-dnn.py
@@ -14,7 +14,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import to_categorical 
-df = pd.read_csv("NBD.csv")
+df = pd.read_csv("diabetes.csv")
 x=df.drop('diabetes',axis=1)
 y=df['diabetes']
 model = Sequential()
